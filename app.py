2890d56ac77b927769d67b403076756b46191f47
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2025-06-19 09:02:00 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 API_KEY = os.getenv("API_KEY_KB")
 local_path = "./leadChatBotDocument/"
-
+#! ============================================================ download all csv file to local path =======================================================
 async def download_All_CSV_Files_to_local_path():
     if not os.path.exists(local_path):
         os.makedirs(local_path)
@@ -32,54 +32,69 @@ async def download_All_CSV_Files_to_local_path():
         print(f"Save sheet: {sheet.title}")
         all_data = sheet.get_all_values()
         csv_file_name = local_path + f"{sheet.title}.csv"
-
         with open(csv_file_name,'w',encoding='utf-8',newline='') as file:
             writer = csv.writer(file)
             writer.writerows(all_data)
     return JSONResponse(content="Done download all Documents into local path!",status_code=200)
-async def upload_All_CSV_FILE_to_Dify():
-    url = 'https://dify.sapocorp.vn/v1/datasets/7f38c77c-e500-4ed7-97d2-83a11d941c82/documents/ecfb0d64-8d68-4e31-9f5a-7b87c9c2fbbc/update-by-file'
-    header = {
-        'Authorization': f"Bearer {API_KEY}",
-    }
+'''
+This is my document_id: d66be3e8-b010-4f3f-aefb-299c4ffa8cbd
+This is name of document: Tư vấn Retail.csv
+This is my document_id: 7832fb85-db35-4162-9bba-e33bb6744de2
+This is name of document: Tư vấn fnb.csv
+This is my document_id: 86c7f394-529d-4e2c-978f-487fa214d704
+This is name of document: Dùng thử Retail.csv
+This is my document_id: fe4787d2-90d3-454f-b5a6-433f661e2f57
+This is name of document: Dùng thử fnb.csv
+This is my document_id: af366da2-b217-41b7-9411-f8b99b23627b
+This is name of document: Bảng giá Retail.csv
+This is my document_id: e7ca0074-c9b8-4cf9-8f6c-8ba05ec543c3
+This is name of document: Bảng giá fnb.csv
+'''
+#! ============================== update dictionary ===============================
 
-    data_field = {
-        "name": "Dify",
-        "indexing_technique": "high_quality",
-        "process_rule": {
-            "rules": {
-                "pre_processing_rules": [    
-                    {"id": "remove_extra_spaces", "enabled": True},
-                    {"id": "remove_urls_emails", "enabled": True}
-                ],
-                "segmentation": {
-                    "separator": "###",      
-                    "max_tokens": 500
+
+async def upload_All_CSV_FILE_to_Dify(dictionary_map_DOCUMENT_NAME_with_DOCUMENT_ID):
+    #! duyệt toàn bộ file để update lên:
+    filenames = os.listdir(local_path)
+    async with httpx.AsyncClient(timeout=10.0) as client:
+        for filename in filenames:
+            if not filename.endswith(".csv"): continue
+            document_id = dictionary_map_DOCUMENT_NAME_with_DOCUMENT_ID.get(filename)
+            print(f"This is my document_id: {document_id}")
+            url = f"https://dify.sapocorp.vn/v1/datasets/aa75acc4-f2ce-41c1-9671-c4d249d1cbdd/documents/{document_id}/update-by-file"
+
+            header = {
+                'Authorization': f"Bearer {API_KEY}",
+            }
+
+            data_field = {
+                "name": f"{filename}",
+                "indexing_technique": "high_quality",
+                "process_rule": {
+                    "rules": {
+                        "pre_processing_rules": [    
+                            {"id": "remove_extra_spaces", "enabled": True},
+                            {"id": "remove_urls_emails", "enabled": True}
+                        ],
+                        "segmentation": {
+                            "separator": "###",      
+                            "max_tokens": 500
+                        }
+                    },
+                    "mode": "custom"
                 }
-            },
-            "mode": "custom"
-        }
-    }
+            }
 
-    # Convert dict to JSON string
-    data_json = json.dumps(data_field)
-
-    #! check if local path dont exist then create it
-    
-
-    #! loop through all files:
-    for filename in os.listdir(local_path):
-        file_path = os.path.join(local_path,filename)
-        if os.path.isfile(file_path):
-            print(f"Pushing file {file_path} to Dify")
+            
+            data_json = json.dumps(data_field) 
+            filePath = os.path.join(local_path,filename)  
             files = {
                 'data': (None, data_json, 'text/plain'),
-                'file': open('./Dùng thử fnb.csv', 'rb')
+                'file': open(filePath, 'rb')
             }
             try:
-                async with httpx.AsyncClient(timeout=10.0) as client:
                     response = await client.post(url = url, headers= header, files = files)
-                    return JSONResponse(content=response.raise_for_status())
+                    print(JSONResponse(content=response))
             except httpx.TimeoutException as timeout:
                 return JSONResponse(content=f"Timeout error: {timeout}")
             except httpx.HTTPError as httpError:
@@ -90,16 +105,25 @@ async def upload_All_CSV_FILE_to_Dify():
                 return JSONResponse(content=f"Network Error: {networkError}")
             except httpx.ConnectError as connectError:
                 return JSONResponse(content=f"Connection Error: {connectError}")
-    return JSONResponse(content="Done udpate all Documents in Dify",status_code=200)
-@app.post("/api/lead/chat/updateDocument")
+            print(JSONResponse(content="Done udpate all Documents in Dify",status_code=200))
+@app.post('/api/lead/chat/updateDocument')
 async def updateDocument():
+
     '''
     1. This function will download all the table in google sheet link
     2. It will save all CSV files in leadChatBOtDocument local path
     3. upload all the files into Dify
     '''
+    dictionary_map_DOCUMENT_NAME_with_DOCUMENT_ID = {
+    "Tư vấn Retail.csv": "d66be3e8-b010-4f3f-aefb-299c4ffa8cbd",
+    "Tư vấn fnb.csv":"7832fb85-db35-4162-9bba-e33bb6744de2",
+    "Dùng thử Retail.csv": "86c7f394-529d-4e2c-978f-487fa214d704",
+    "Dùng thử fnb.csv": "fe4787d2-90d3-454f-b5a6-433f661e2f57",
+    "Bảng giá Retail.csv": "af366da2-b217-41b7-9411-f8b99b23627b",
+    "Bảng giá fnb.csv": "e7ca0074-c9b8-4cf9-8f6c-8ba05ec543c3"
+    }
     await download_All_CSV_Files_to_local_path()
-    await upload_All_CSV_FILE_to_Dify()
+    await upload_All_CSV_FILE_to_Dify(dictionary_map_DOCUMENT_NAME_with_DOCUMENT_ID)
     return JSONResponse(content=f"Process done in download all CSV files and update all CSV files",status_code=200)
 
 
